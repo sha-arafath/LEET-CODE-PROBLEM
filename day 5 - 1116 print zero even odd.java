bf9fd52ec97ec5e3628ef806da822964fb44aee2
sha-arafath <sharafa2708@gmail.com>
2026-07-28 10:48:04 +0530

    Create day 5 - 1116 print zero even odd.java

---
+++ day 5 - 1116 print zero even odd.java
@@ -0,0 +1,45 @@
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+class ZeroEvenOdd {
+    private int n;
+    private Semaphore zeroSem;
+    private Semaphore evenSem;
+    private Semaphore oddSem;
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+        this.zeroSem = new Semaphore(1);
+        this.evenSem = new Semaphore(0);
+        this.oddSem = new Semaphore(0);
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            zeroSem.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 1) {
+                oddSem.release();
+            } else {
+                evenSem.release();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            evenSem.acquire();
+            printNumber.accept(i);
+            zeroSem.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            oddSem.acquire();
+            printNumber.accept(i);
+            zeroSem.release();
+        }
+    }
+}
